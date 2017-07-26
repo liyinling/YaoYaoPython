d52c63f597fcce5bff521acdde4bda88170f9c50
liyinling <253002616@qq.com>
2017-07-26 22:44:09 +0800

    Add files via upload
    练习1+附加题

---
+++ python01/ex1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*- ##如果出现中文字体，就应该加改行，不管中文字是不是出现在注解中
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said"do not touch this.'
+##附加题1
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said"do not touch this.'
+## 附加题1：
+print ""  ##让你的脚本多打印一行
+
+##附加题2
+# -*- coding: utf-8 -*-
+print "Hello World!"
+# print "Hello Again"
+# print "I like typing this."
+# print "This is fun."
+# print 'Yay! Printing.'
+# print "I'd much rather you 'not'."
+# print 'I "said"do not touch this.' 井号的作用就是使改行变成注释，不显示
