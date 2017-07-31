07fae9702e040685eaed3df600b7e90a8bc5503f
liyinling <253002616@qq.com>
2017-07-31 23:51:05 +0800

    Update ex3.py

--- python03/ex3.py
+++ python03/ex3.py
@@ -1,1 +1,29 @@
-
+# -*- coding: utf-8 -*-
+# 屏幕上显示我将数我有几只鸡？
+print "I will now count my chickens:"
+# 显示 母鸡30
+print "Hens",25 + 30 / 6
+# 显示 公鸡97（%是指取余数的意思）
+print "Rooster",100 - 25 * 3 % 4
+# 显示 现在我将数一下有几只鸡蛋？
+print "Now I will count the eggs:"
+# 显示 7（1/4=0）
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# 显示 3 + 2 < 5 - 7 是真的吗？
+print "Is it true that 3 + 2 < 5 - 7?"
+# 显示 错误
+print 3 + 2 < 5 - 7
+# 显示 What is 3 + 2？5
+print "What is 3 + 2?", 3 + 2
+# 显示 What is 5 - 7？-2
+print "What is 5 - 7?", 5 - 7
+# 显示 那是错误的
+print "Oh,that's why it's False."
+# 显示 现在来了解更多
+print "Now about some more."
+# 显示 Is it greater？True
+print "Is it greater?", 5 > -2
+# 显示 Is it greater？False
+print "Is it greater or equal?", 5 >= -2
+# 显示 Is it less or equal？False
+print "Is it less or equal?", 5 <= -2
