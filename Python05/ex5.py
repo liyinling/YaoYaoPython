c414f4d53d09f454f0216e6ad94213ac801ad2c6
liyinling <253002616@qq.com>
2017-08-02 23:38:30 +0800

    Update ex5.py

--- Python05/ex5.py
+++ Python05/ex5.py
@@ -1,3 +1,4 @@
+# -- coding: utf-8 --
 my_name='Zed A. Shaw'
 my_age= 35
 my_height= 74
@@ -15,3 +16,22 @@ print "His teeth are usually %s depending on the coffee." % my_teeth
 
 print "If I add %d,%d,and %d I get %d." %(my_age,my_height,my_weight,my_age + my_height + my_weight)
 
+# 附加题1
+name='Zed A. Shaw'
+age= 35
+height= 74
+cm = height * 2.54
+weight = 180
+kg = 0.4535924 * weight
+eyes='Blue'
+teeth='White'
+hair='Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % cm
+print "He's %d pounds heavy." % kg
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." %(eyes,hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+print "If I add %d,%d,and %d I get %d." %(age,height,weight,age + height + weight)
